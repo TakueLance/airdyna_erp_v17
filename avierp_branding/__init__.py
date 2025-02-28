bd2efbf95982380868244e5953084c2b5daeb565
TakueBere <45051894+TakueLance@users.noreply.github.com>
2025-02-28 14:49:11 +0200

    Delete avierp_branding directory

--- avierp_branding/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import models
-from . import controllers
-from .hooks import post_init_debranding, uninstall_debranding
--- avierp_branding/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import ir_ui_view,ir_http,res_config_settings,res_users
